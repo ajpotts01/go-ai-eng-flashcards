d0775d3f169a8bc42a05e742c9e952b79e9ead27
Alexander Potts <ajpotts01@gmail.com>
2025-08-16 23:40:01 +1000

    fix(backend): don't require godotenv

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -13,9 +10,9 @@ type Config struct {
 }
 
 func Load() *Config {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found or error loading .env file")
-	}
+	//if err := godotenv.Load(); err != nil {
+	//	log.Println("No .env file found or error loading .env file")
+	//}
 
 	config := &Config{
 		DatabaseURL: getEnv("DB_URL"),
